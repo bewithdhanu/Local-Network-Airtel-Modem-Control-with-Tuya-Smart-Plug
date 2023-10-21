d0125269df882c74d5059912a2b159b4841a8322
Dhanu K <bewithdhanu@gmail.com>
2023-10-21 15:39:03 +0530

    add extra conditions to turnoff anf turnon

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import time
 
-from device import turnOff, turnOn
+from device import turnOff, turnOn, getStatus
 from modem import getBatteryPercent
 
 while True:
     battery = getBatteryPercent()
-    if battery > 80:
+    if battery > 80 and getStatus():
         turnOff()
-    if battery < 20:
+    if battery < 20 and not getStatus():
         turnOn()
 
     # Wait for 60 seconds (1 minute) before running the task again
