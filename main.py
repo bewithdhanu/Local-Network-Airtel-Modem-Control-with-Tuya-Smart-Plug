2f8f0b8e9678f471164da2e310a154348c18a618
Dhanu K <bewithdhanu@gmail.com>
2023-10-22 13:08:29 +0530

    wait for wifi connection and then continue

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 import time
+import os
+import subprocess
 
 from device import turnOff, turnOn, getStatus
 from modem import getBatteryPercent
 
+
+def is_wifi_connected():
+    try:
+        # Use the 'iwgetid' command to check if the system is connected to Wi-Fi
+        subprocess.check_output(["iwgetid"])
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+
+def wait_for_wifi_connection(timeout=300):
+    start_time = time.time()
+    while not is_wifi_connected():
+        if time.time() - start_time >= timeout:
+            print("Timeout reached. Wi-Fi not connected.")
+            break
+        time.sleep(1)
+
+
+wait_for_wifi_connection()
 while True:
     battery = getBatteryPercent()
-    if battery > 80 and getStatus():
-        turnOff()
-    if battery < 20 and not getStatus():
-        turnOn()
+    if battery is not None:
+        if battery > 80 and getStatus():
+            turnOff()
+        if battery < 20 and not getStatus():
+            turnOn()
 
     # Wait for 60 seconds (1 minute) before running the task again
     time.sleep(60)
--- modem.py
+++ modem.py
@@ -3,23 +3,25 @@ import json
 import requests
 import xmltodict
 
-url = "http://192.168.1.1/mark_title.w.xml"
-
-payload = {}
-headers = {
-    'Referer': 'http://192.168.1.1/index.html'
-}
-
-response = requests.request("GET", url, headers=headers, data=payload)
-
-# Parse the XML data to a Python dictionary
-xml_dict = xmltodict.parse(response.text)
-
-# Convert the dictionary to JSON
-json_data = json.dumps(xml_dict, indent=4)
-
-data_dict = json.loads(json_data)
-
 
 def getBatteryPercent():
-    return int(data_dict['title']['batt_p'])
+    try:
+        url = "http://192.168.1.1/mark_title.w.xml"
+
+        payload = {}
+        headers = {
+            'Referer': 'http://192.168.1.1/index.html'
+        }
+
+        response = requests.request("GET", url, headers=headers, data=payload)
+
+        # Parse the XML data to a Python dictionary
+        xml_dict = xmltodict.parse(response.text)
+
+        # Convert the dictionary to JSON
+        json_data = json.dumps(xml_dict, indent=4)
+
+        data_dict = json.loads(json_data)
+        return int(data_dict['title']['batt_p'])
+    finally:
+        return None
